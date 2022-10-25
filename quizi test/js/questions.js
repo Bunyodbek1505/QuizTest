0e5d172be7e47bfdf42bc909a2b0a8cb82fa38ba
Bunyodbek <99537396+Bunyodbek1505@users.noreply.github.com>
2022-10-25 11:02:15 +0100

    Add files via upload

---
+++ quizi test/js/questions.js
@@ -0,0 +1,57 @@
+let questions = [
+      {
+          numb: 1,
+          question: "Ozbekistonning poytaxti?",
+          answer: "Toshkent",
+          options: [
+              "Baku",
+              "Toshkent",
+              "Buxarest",
+              "Madrid"
+          ]
+      },
+      {
+          numb: 2,
+          question: "Taekwondo bo'yicha oxirgi olimpiada chempioni..",
+          answer: "Ulug'bek Rashitov",
+          options: [
+              "Dmitriy Shoking",
+              "Nikita Rafalovich",
+              "Ulug'bek Rashitov",
+              "Hechkim"
+          ]
+      },
+      {
+          numb: 3,
+          question: "HTML nima?",
+          answer: "Hyper Text Markup Language",
+          options: [
+              "Hyper Trainer Marking Language",
+              "Hyper Text Markup Leveler",
+              "Hyper Text Marketing Language",
+              "Hyper Text Markup Language"
+          ]
+      },
+      {
+          numb: 4,
+          question: "JavaScript fayl HTMLda qanday ulanadi?",
+          answer: "script",
+          options: [
+              "link",
+              "p",
+              "script",
+              "div"
+          ]
+      },
+      {
+          numb: 5,
+          question: "# belgisi qanaqa selektor ekanligini bildiradi?",
+          answer: "id",
+          options: [
+              "html",
+              "tag",
+              "class",
+              "id"
+          ]
+      },
+  ];
